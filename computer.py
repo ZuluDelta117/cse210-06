3419aa004a9a34c53fd90cafd2625c2ed57033fc
americanfootball99 <113465103+americanfootball99@users.noreply.github.com>
2022-12-09 20:56:45 -0700

    Cleaned up

--- computer.py
+++ computer.py
@@ -5,8 +5,6 @@ class Computer:
     def __init__(self):
         self._base_risk = .5
         self._messages = ['Drat! I busted', 'Yay! I scored over 1000 points', 'Haha, you busted']
-        #self._total_points = int(0)
-        #self._round_points = int(0)
 
     def return_risk(self):
         return self._base_risk
@@ -14,11 +12,6 @@ class Computer:
     def return_message(self, num):
         return self._messages[num-1]
 
-   # def return_total_points(self):
-    #    return self._total_points
-
-    #def modify_total_points(self, value):
-     #   self._total_points =+ value
 
     def modify_risk(self, change):
         self._base_risk = self._base_risk * change
